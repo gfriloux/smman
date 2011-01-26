455e282537d8690bfebf9348b765df758f741f5d
kuri igen <kuri@e4.enlightenment.org>
2011-01-26 08:46:17 +0100

    I forgot eina_counter_start when c/c code

--- src/bin/spy.c
+++ src/bin/spy.c
@@ -141,7 +141,7 @@ int spy_event(void *data, Ecore_File_Monitor *em, Ecore_File_Event event, const 
 		char *counterresult;
 		if( !log )
 			return(ECORE_CALLBACK_RENEW);
-
+		eina_counter_start(counter);
 		// If logfile got trunc, than size is different
 		logfiles_getsize((char *)path, &filesize);
 
