717627b5db5d0071771323fc1b8d74f7ef48a8aa
Guillaume Friloux <guillaume.friloux@gmail.com>
2013-12-26 13:58:06 +0100

    Update makefile

--- src/bin/Makefile.mk
+++ src/bin/Makefile.mk
@@ -9,10 +9,14 @@ src/bin/main.c \
 src/bin/config.c \
 src/bin/filter.c \
 src/bin/log.c \
+src/bin/utils.c \
 src/bin/smman.h
 src_bin_smman_CPPFLAGS = @BIN_CFLAGS@ $(EXTRA_CPPFLAGS)
 src_bin_smman_LDFLAGS = @BIN_LIBS@
 src_bin_smman_LDADD = \
 src/lib/libconf.la \
 src/lib/librules.la \
-src/lib/libspy.la
+src/lib/libspy.la \
+src/lib/libstore.la \
+src/lib/libcjson.la
+
