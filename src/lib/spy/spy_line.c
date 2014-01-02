9f805a152e0d77646b962e83c3d17cc0113477c7
Guillaume Friloux <guillaume.friloux@gmail.com>
2014-01-02 11:03:55 +0100

    Improve documentation.

--- src/lib/spy/spy_line.c
+++ src/lib/spy/spy_line.c
@@ -5,12 +5,25 @@
  * @{
  */
 
+/**
+ * @brief Returns the line parsed by spy.
+ * @param sl Spy_Line structure.
+ * @return Pointer to parsed line.
+ *
+ * The pointer returned is the internal pointer of the Spy_Line,
+ * so, do not free it!
+ */
 const char *
 spy_line_get(Spy_Line *sl)
 {
    return sl->line;
 }
 
+/**
+ * @brief Return the Spy_File of a Spy_Line.
+ * @param sl Spy_Line structure.
+ * @return Pointer to the Spy_File structure.
+ */
 Spy_File *
 spy_line_spyfile_get(Spy_Line *sl)
 {
